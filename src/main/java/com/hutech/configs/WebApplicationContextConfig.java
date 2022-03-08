8400715d029d60b1e223ed550d901373d49dfde1
hoanghiep3110 <pthh3110@gmail.com>
2022-03-08 14:15:59 +0700

    Initial commit

---
+++ src/main/java/com/hutech/configs/WebApplicationContextConfig.java
@@ -0,0 +1,36 @@
+package com.hutech.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ *
+ * @author Hiệp Phan
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.hutech.controllers")
+public class WebApplicationContextConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resource = new InternalResourceViewResolver();
+
+        resource.setViewClass(JstlView.class);
+        resource.setPrefix("/WEB-INF/jsp/");
+        resource.setSuffix(".jsp");
+
+        return resource;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index.jsp
+    Created on : 8 Mar 2022, 1:46:54 pm
+    Author     : Hiệp Phan
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>HomePage</title>
+    </head>
+    <body>
+        <h1>Xin chào</h1>
+    </body>
+</html>
